1f05d817c150c3837ade3180ab693f579724fcbb
Linked Matrix <afifak14@gmail.com>
2025-04-04 16:28:27 +0500

    Dockerized the app

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test',
+        'USER': 'test',
+        'PASSWORD': 'test',
+        'HOST': 'db',   # service name for docker-compose
+        'PORT': 5432,
     }
 }
 
